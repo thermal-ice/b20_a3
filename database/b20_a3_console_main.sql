c6f610855eb6dca9e024df4172eb9bbfa1606b98
sujiayi317 <sujiayi317@gmail.com>
2021-03-27 17:12:09 -0400

    Add predefined data into our database

---
+++ database/b20_a3_console_main.sql
@@ -0,0 +1,75 @@
+CREATE TABLE Feedback(
+    feedback_id INTEGER PRIMARY KEY,
+    feedback_text TEXT NOT NULL
+
+);
+
+CREATE TABLE Instructor(
+    instructor_id INTEGER PRIMARY KEY NOT NULL UNIQUE ,
+    username TEXT NOT NULL ,
+    password TEXT NOT NULL,
+    lecture_section INTEGER NOT NULL
+
+);
+
+CREATE TABLE Student(
+    student_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL ,
+    password TEXT NOT NULL ,
+    firstName TEXT NOT NULL ,
+    lastName TEXT NOT NULL ,
+    lectureSection INTEGER,
+    assignmentMarks TEXT,
+    labMarks TEXT,
+    midtermMark INTEGER ,
+    finalExam INTEGER
+);
+
+CREATE TABLE Remarks(
+    student_id INTEGER,
+    assignmentType TEXT NOT NULL ,
+    remarkMessage TEXT NOT NULL ,
+    FOREIGN KEY (student_id) references Student(student_id)
+);
+
+
+
+
+
+INSERT INTO Feedback(feedback_text) VALUES ('We need more class engagement');
+INSERT INTO Feedback(feedback_text) VALUES ('We need more interesting TA');
+INSERT INTO Feedback(feedback_text) VALUES ('Instructor could set up breakout rooms');
+INSERT INTO Feedback(feedback_text) VALUES ('The assignment is too long');
+INSERT INTO Feedback(feedback_text) VALUES ('Time is not sufficient for our midterm');
+
+
+INSERT INTO Instructor VALUES (10000, 'LyndaBarnes', 'GraduateOffice', 1);
+INSERT INTO Instructor VALUES (10001, 'SteveEngels', 'sengels', 1);
+INSERT INTO Instructor VALUES (10002, 'PaulGries', 'pgries', 2);
+INSERT INTO Instructor VALUES (10003, 'DanHeap', 'heapify', 2);
+INSERT INTO Instructor VALUES (10004, 'KarenReid', 'reid', 3);
+
+INSERT INTO Student VALUES (00128, 'Zhang', 'Zhang00', 'Larry', 'Zhang',2, 86, 94, 86, 95);
+INSERT INTO Student VALUES (00345, 'Shankar', 'Shankar00', 'Tom', 'Shankar',2, 75,98, 85, 75);
+INSERT INTO Student VALUES (00991,'Brandt', 'Brandt00', 'Dan', 'Brandt', 1, 80, 97, 78, 85);
+INSERT INTO Student VALUES (00121, 'Chavez', 'Chavez00', 'Tarek', 'Chavez',1, 90,92, 85, 96);
+INSERT INTO Student VALUES (00553, 'Peltier', 'Peltier00', 'Ashton', 'Peltier',2, 69,86, 85, 79);
+INSERT INTO Student VALUES (00678, 'Levy', 'Levy00', 'Ravin', 'Levy',3, 84,95,87, 90);
+INSERT INTO Student VALUES (00321, 'Williams', 'Williams00', 'Allan','Williams', 1, 94,74,79,93);
+INSERT INTO Student VALUES (00739, 'Sanchez', 'Sanchez00', 'Michael', 'Sanchez',3, 87, 75,83,78);
+INSERT INTO Student VALUES (00557, 'Snow', 'Snow00', 'White', 'Snow', 3, 92, 95, 89, 93);
+INSERT INTO Student VALUES (00543, 'Brown', 'Brown00', 'Marsha', 'Brown', 2, 88,78,92, 79);
+
+INSERT INTO Remarks VALUES (00128, 'A2', 'I want more marks');
+INSERT INTO Remarks VALUES (00128, 'Midterm', 'I want even more marks');
+INSERT INTO Remarks VALUES (00991, 'A2', 'My mark is not enough');
+INSERT INTO Remarks VALUES (00128, 'A1', 'The total score is not calculated correctly');
+INSERT INTO Remarks VALUES (00128, 'Midterm', 'I want more marks for question 5');
+
+SELECT * from Feedback;
+
+
+
+
+
+
