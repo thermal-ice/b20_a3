ebedfc1d956a400ba5ecf7f5fc2dc5a0f1bc0d8d
Carlos Huang <thebacondragon@gmail.com>
2021-03-29 14:41:49 -0400

    Finished some of the user stories for Instructors

--- app.py
+++ app.py
@@ -8,11 +8,20 @@ app = Flask(__name__)
 # Setting the path to the database file
 DATABASE = os.path.join('.', 'database', 'a3_database')
 
+def scoreStringParser(scoreString: str):
+    return scoreString.split('/')
+
+# Passing in the parser function for assignment/lab marks
+app.jinja_env.globals.update(scoreStringParser = scoreStringParser)
+
+
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+
+    db.row_factory = make_dicts
     return db
 
 
@@ -21,7 +30,6 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-
 
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
@@ -35,10 +43,28 @@ def close_connection(exception):
         db.close()
 
 
+
+
+
 @app.route('/')
 def root():
     result = query_db("select * from Instructor")
     return result.__str__()
+
+
+@app.route('/studentmarks')
+def getStudentMarks():
+    return query_db('SELECT * FROM Student').__str__()
+
+
+@app.route('/feedback')
+def getAllFeedback():
+    return query_db('SELECT * FROM Feedback').__str__()
+
+@app.route('/remarks')
+def getAllRemarks():
+    return query_db('SELECT * FROM Remarks').__str__()
+
 
 
 if __name__ == "__main__":
